e6ab91d4a4ff1d7c4524ba7f3097786bfecc5137
YY-Yoru <eirairumataruyuutirainenn@gmail.com>
2025-09-23 22:36:32 +0700

    Update gIndex.config.ts

--- src/config/gIndex.config.ts
+++ src/config/gIndex.config.ts
@@ -17,7 +17,7 @@ const config: z.input<typeof Schema_Config> = {
    * @default process.env.NEXT_PUBLIC_DOMAIN
    * @fallback process.env.NEXT_PUBLIC_VERCEL_URL
    */
-  basePath: IS_DEV ? "http://localhost:3000" : `https://${BASE_URL}`,
+  basePath: IS_DEV ? "http://localhost:7860" : `https://${BASE_URL}`,
 
   /**
    * Show deploy guide dropdown on navbar
